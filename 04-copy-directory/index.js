73aed3796091c0149a6a04760b3b35742752c744
DeguzBelarus <deguz@mail.ru>
2022-11-04 16:54:12 +0300

    feat: complete the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const { readdir } = require("fs/promises");
 
+(async function () {
+  const sourcePath = path.join(__dirname, "files");
+  const destinationPath = path.join(__dirname, "files-copy");
+
+  fs.mkdir(destinationPath, { recursive: true }, (error) => {
+    console.log("Creating a folder files-copy...");
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The files-copy folder has been created!");
+  });
+
+  const files = await readdir(sourcePath, {
+    withFileTypes: true,
+  });
+
+  let currentFile = 0;
+  for (const file of files) {
+    fs.copyFile(
+      path.join(sourcePath, file.name),
+      path.join(destinationPath, file.name),
+      (error) => {
+        currentFile++;
+        console.log(`Copying a file ${file.name}...`);
+        if (error) {
+          return console.log(error);
+        }
+        if (currentFile === files.length) {
+          console.log("All files have been copied!");
+        }
+      }
+    );
+  }
+})();
